cfd2103f919c4112df407ef2d9d7b3d2f7eac5b3
JorgeRochaA <jor2019.cr@gmail.com>
2022-10-05 22:11:37 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/Currency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public partial class Currency
+    {
+        public Currency()
+        {
+            Inventories = new HashSet<Inventory>();
+        }
+
+        public int Idcurrency { get; set; }
+        public string CurrencyCode { get; set; } = null!;
+        public string CurrencyName { get; set; } = null!;
+        public bool? Active { get; set; }
+
+        public virtual ICollection<Inventory> Inventories { get; set; }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var connection = "SERVER=.\\SQLEXPRESS01;DATABASE=P6SHOPPING;INTEGRATED SECURITY=TRUE;User Id=;Password=;";
+
+        builder.Services.AddDbContext<P6SHOPPINGContext>(options => options.UseSqlServer(connection));
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
